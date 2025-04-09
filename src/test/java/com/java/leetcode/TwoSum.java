698b00cc643748e65d0dbe73c64670cc883fc6ff
Koteswararao Majji <krmajji@gmail.com>
2025-04-09 22:28:04 +0530

    two sum program using hashmap and package for API test added

---
+++ src/test/java/com/java/leetcode/TwoSum.java
@@ -0,0 +1,35 @@
+package com.java.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		
+		 int[] nums = {2, 7, 11, 15};
+	        int target = 9;
+
+	        int[] result = twoSum(nums, target);
+	        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+		
+
+	}
+	
+	public static int[] twoSum (int[] input, int target) {
+		
+		HashMap<Integer, Integer> arrayMap = new HashMap<>();
+		for (int i=0;i<input.length;i++) {
+			
+			int complement = target-input[i];
+			
+			if (arrayMap.containsKey(complement)) {
+				return new int[] {arrayMap.get(complement), i};
+			}
+			
+			arrayMap.put(input[i], i);
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+}
